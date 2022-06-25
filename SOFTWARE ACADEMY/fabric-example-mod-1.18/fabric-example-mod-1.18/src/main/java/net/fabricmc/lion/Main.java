25726e8e6c00a0638b80f0bc3bf9a03f8575793f
Maribel1988 <102556943+Maribel1988@users.noreply.github.com>
2022-06-25 05:18:51 +0100

    Add files via upload

---
+++ SOFTWARE ACADEMY/fabric-example-mod-1.18/fabric-example-mod-1.18/src/main/java/net/fabricmc/lion/Main.java
@@ -0,0 +1,29 @@
+package net.fabricmc.lion;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main implements ModInitializer {
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger("lionmod");
+
+	public static final Item LIGHTNING_AXE = new Item(new FabricItemSettings().group(ItemGroup.COMBAT));
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Hello Fabric world!");
+
+		Registry.register(Registry.ITEM, new Identifier("lionmod","lightning_axe"), LIGHTNING_AXE);
+	}
+}
---
+++ SOFTWARE ACADEMY/fabric-example-mod-1.18/fabric-example-mod-1.18/src/main/java/net/fabricmc/lion/mixin/LionMixin.java
@@ -0,0 +1,16 @@
+package net.fabricmc.lion.mixin;
+
+import net.fabricmc.lion.Main;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class LionMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		Main.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
